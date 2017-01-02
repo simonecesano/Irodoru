6f6db0f85cd413b22ab72a77cc335a061df37cb4
Simone Cesano <simonecesano@libero.it>
2017-01-02 15:17:23 +0100

    eliminated lots of logging

--- obj_clicker.js
+++ obj_clicker.js
@@ -1,18 +1,15 @@
+var idw = new Iredaware(scene);
+
 renderer.domElement.addEventListener('mousedown', function(event) {
-    // console.log({ pageX: event.pageX,
-    // 		  pageY: event.pageY,
-    // 		  devicePixelRatio: devicePixelRatio,
-    // 		  offsetLeft: this.offsetLeft,
-    // 		  offsetTop: this.offsetTop
-    // 		})
+    console.log(event);
+
+    
     var vector = new THREE.Vector3(
           (event.pageX - this.offsetLeft) / this.width * 2 - 1,
         - (event.pageY - this.offsetTop)  / this.height * 2 + 1,
         0
     );
-    // console.log(vector)
     vector.unproject(camera);
-    // console.log(vector)
     var raycaster = new THREE.Raycaster(
         camera.position,
         vector.sub(camera.position).normalize()
@@ -20,12 +17,16 @@ renderer.domElement.addEventListener('mousedown', function(event) {
 
     var intersects = raycaster.intersectObjects(obj.children);
     if (intersects.length) {
-        // console.log(intersects.length);
-	// console.log(intersects[1].object.id);
 	var r = Math.random(); var g = Math.random(); var b = Math.random();
 	var c = chroma(r * 256, g * 256, b * 256);
 	intersects[0].object.material.color.setRGB (r, g, b);
+	console.log(intersects[0].object.id)
+	idw.toggleSelect(intersects[0].object.id, event.shiftKey);
+	console.log(idw);
     } else {
 	console.log('nothing here'); console.log(vector);
     }
 }, false);
+
+function objectAtClick(event) {
+}
